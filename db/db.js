a323e4559af1ff4450c97c56ac2063ec5fb00928
Ismail Taher <ismail.taher@ymail.com>
2025-08-23 06:26:30 +0500

    bind objection Model to knex (db.js)

--- db/db.js
+++ db/db.js
@@ -1,8 +1,12 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile');
+require('dotenv').config();
+const { Model } = require('objection');
 
 const environment = process.env.NODE_ENV || 'development';
 
 const db = knex(knexConfig[environment]);
+
+Model.knex(db);
 
 module.exports = db;
