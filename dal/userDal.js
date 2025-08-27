5bcc1891c8aa7368b375c49142ba88c336e5c0df
Ismail Taher <ismail.taher@ymail.com>
2025-08-27 19:23:55 +0500

    update dals/userDal.js

--- dal/userDal.js
+++ dal/userDal.js
@@ -1,45 +1,42 @@
-const knex = require('../db/db');
+const User = require('../db/models/User');
 
 // registerController logic
 const findUserByUsername = async (username) => {
-  const user = await knex('users').where({ username }).first();
+  const user = await User.query().findOne({ username });
   return user;
 };
 
 const createUser = async (username, hashedPassword, trx) => {
-  const [id] = await trx('users')
-    .insert({ username: username, password: hashedPassword })
-    .returning('id');
-  return typeof id === 'object' ? id.id : id;
+  const user = await User.query(trx).insert({
+    username,
+    password: hashedPassword,
+  });
+  return user.id;
 };
 
 const assignRoleToUser = async (userId, roleId, trx) => {
-  await trx('user_roles').insert({ user_id: userId, role_id: roleId });
+  await User.relatedQuery('roles', trx).for(userId).relate(roleId);
 };
 
 // authController logic
 const getUserRoles = async (userId) => {
-  const roles = await knex('user_roles')
-    .select('role_id')
-    .where({ user_id: userId });
-  return roles.map((row) => row.role_id);
+  const roles = await User.relatedQuery('roles').for(userId).select('roles.id');
+  return roles.map((row) => row.id);
 };
 
 const assignRefreshTokenToUser = async (userId, refreshToken) => {
-  await knex('users')
-    .where({ id: userId })
-    .update({ refresh_token: refreshToken });
+  await User.query().findById(userId).patch({ refresh_token: refreshToken });
 };
 
 // refreshController & logoutController logic
 const findUserByRefreshToken = async (refreshToken) => {
-  const user = knex('users').where({ refresh_token: refreshToken }).first();
+  const user = User.query().findOne({ refresh_token: refreshToken });
   return user;
 };
 
 // logoutController logic
 const deleteRefreshToken = async (userId) => {
-  await knex('users').where({ id: userId }).update({ refresh_token: null });
+  await User.query().findById(userId).patch({ refresh_token: null });
 };
 
 module.exports = {
